1aef1543d267be8b69959c319bf8096b62ef7bd9
Umur Can <>
2024-01-24 16:51:16 +0300

    updated finger sim

--- Scripts/Helpers/FingerSimulator.cs
+++ Scripts/Helpers/FingerSimulator.cs
@@ -1,5 +1,4 @@
-﻿// using DG.Tweening;
-using UnityEngine;
+﻿using UnityEngine;
 
 namespace Helpers
 {
@@ -13,40 +12,45 @@ namespace Helpers
         public float ActiveScale = 0.5f;
         public float ScaleDuration = 0.1f;
         public KeyCode ScaleKey = KeyCode.LeftControl;
-        // public Ease ScaleEase = Ease.OutQuad;
         private bool fingerScaled;
 
 
         private Vector3 _originalScale;
         private RectTransform _rectTransform;
-    
+        
+        private Vector3 aimScale;
+        private float scaleSpeed;
+
         public void Start()
         {
             _rectTransform = GetComponent<RectTransform>();
             _originalScale = _rectTransform.localScale;
-        
+            aimScale = _originalScale;
+            
+            //Calculate scale speed from original scale and active scale
+            scaleSpeed = (ActiveScale - _originalScale.x) / ScaleDuration; 
+
         }
 
         public void Update()
         {
+            _rectTransform.transform.localScale = Vector3.MoveTowards(_rectTransform.transform.localScale, aimScale, Time.deltaTime * scaleSpeed);
+            
             if (Input.GetKey(FollowKey))
             {
                 _rectTransform.position = Vector2.Lerp(_rectTransform.position, Input.mousePosition,Time.deltaTime*FollowSpeed);
             }
 
-            // if (!fingerScaled && Input.GetKeyDown(ScaleKey))
-            // {
-            //     DOTween.Kill(_rectTransform);
-            //     fingerScaled = true;
-            //     _rectTransform.DOScale(ActiveScale * Vector3.one, ScaleDuration).SetEase(ScaleEase);
-            // }else if (fingerScaled && Input.GetKeyUp(ScaleKey))
-            // {
-            //     DOTween.Kill(_rectTransform);
-            //     _rectTransform.DOScale(_originalScale, ScaleDuration).SetEase(ScaleEase);
-            //     fingerScaled = false;
-            // }
+            if (!fingerScaled && Input.GetKeyDown(ScaleKey))
+            {
+                fingerScaled = true;
+                aimScale = ActiveScale * Vector3.one;
+            }else if (fingerScaled && Input.GetKeyUp(ScaleKey))
+            {
+             
+                aimScale = _originalScale;
+                fingerScaled = false;
+            }
         }
-    
-
     }
 }
