a7e85442a60d4e6a48fd24e8b908ce9cef28fc99
Umur Can <>
2024-10-15 16:51:52 +0300

    AnalyticInitializer.cs

---
+++ Scripts/Helpers/AnalyticInitializer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using Core.Interfaces;
+using UnityEngine;
+
+namespace CorePublic.Helpers
+{
+    public abstract class AnalyticInitializer: MonoBehaviour, IPrerequisite
+    {
+        public AnalyticInitializer[] dependencies;
+        public bool initializeAtStart = true;
+        public abstract bool IsInitialized();
+        public abstract void Initialize();
+
+        private IEnumerator Start()
+        {
+            if (initializeAtStart)
+            {
+                foreach (AnalyticInitializer dependency in dependencies)
+                {
+                    while (!dependency.IsInitialized())
+                    {
+                        yield return null;
+                    }
+                }
+                Initialize();
+            }
+        }
+
+        public bool IsMet()
+        {
+            return IsInitialized();
+        }
+    }
+}
