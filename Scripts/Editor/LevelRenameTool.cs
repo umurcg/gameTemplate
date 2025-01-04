682e3ace6e2d6031645c3f98c92b0ed3d9315d48
Umur Can <>
2025-01-04 13:08:27 +0300

    Added scroll bar to level rename tool

--- Scripts/Editor/LevelRenameTool.cs
+++ Scripts/Editor/LevelRenameTool.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 
 namespace CorePublic.Editor
 {
-    
-    public class LevelRenameTool:EditorWindow
+    public class LevelRenameTool : EditorWindow
     {
-        
         public string namePrefix;
         public LevelData[] levels;
         public int startIndex;
+
+        // Add a Vector2 to track the scroll position
+        private Vector2 scrollPos;
         
         [MenuItem("Reboot/Tools/Level Rename Tool")]
         public static void OpenWindow()
@@ -21,23 +22,25 @@ namespace CorePublic.Editor
         
         private void OnGUI()
         {
+            // Begin the scroll view block
+            scrollPos = EditorGUILayout.BeginScrollView(scrollPos, GUILayout.ExpandHeight(true));
+
             EditorGUILayout.LabelField("Rename Levels", EditorStyles.boldLabel);
             EditorGUILayout.Space();
-            
-            
-            //Draw gameobject array
+
+            // Draw gameobject array
             SerializedObject so = new SerializedObject(this);
             SerializedProperty levelsProperty = so.FindProperty("levels");
             EditorGUILayout.PropertyField(levelsProperty, true);
             so.ApplyModifiedProperties();
-            
+
             EditorGUILayout.Space();
             namePrefix = EditorGUILayout.TextField("Name Prefix", namePrefix);
             EditorGUILayout.Space();
-            
+
             startIndex = EditorGUILayout.IntField("Start Index", startIndex);
             EditorGUILayout.Space();
-            
+
             if (GUILayout.Button("Get Levels"))
             {
                 GetLevelsFromLevelManager();
@@ -47,6 +50,9 @@ namespace CorePublic.Editor
             {
                 RenameLevels();
             }
+
+            // End the scroll view block
+            EditorGUILayout.EndScrollView();
         }
 
         public void GetLevelsFromLevelManager()
@@ -57,32 +63,32 @@ namespace CorePublic.Editor
         
         private void RenameLevels()
         {
-            //First rename files to temp names to avoid conflicts
+            // First rename files to temp names to avoid conflicts
             for (int index = 0; index < levels.Length; index++)
             {
                 LevelData level = levels[index];
-                //Get path of the asset
+                // Get path of the asset
                 string path = AssetDatabase.GetAssetPath(level);
-                //Random name to avoid conflicts
+                // Random name to avoid conflicts
                 string newName = "temp" + index;
                 AssetDatabase.RenameAsset(path, newName);
 
-                //Save the changes
+                // Save the changes
                 AssetDatabase.SaveAssets();
             }
-            
-            
+
+            // Then rename files to their final names
             for (int index = 0; index < levels.Length; index++)
             {
                 LevelData level = levels[index];
-                //Get path of the asset
+                // Get path of the asset
                 string path = AssetDatabase.GetAssetPath(level);
 
-                //Rename the level
+                // Rename the level
                 string newName = namePrefix + (index + startIndex);
                 AssetDatabase.RenameAsset(path, newName);
 
-                //Save the changes
+                // Save the changes
                 AssetDatabase.SaveAssets();
             }
         }
