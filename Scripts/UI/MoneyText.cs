f1a0721dab2c7c759ee10c010c2c6752aaf136b6
Umur Can <>
2025-09-13 10:46:57 +0300

    Add UnityEvents for money visibility changes in MoneyText class

--- Scripts/UI/MoneyText.cs
+++ Scripts/UI/MoneyText.cs
@@ -1,5 +1,6 @@
 ﻿using CorePublic.Managers;
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace CorePublic.UI
 {
@@ -18,51 +19,78 @@ namespace CorePublic.UI
         public Types type;
         private float offset;
 
+        private float _lastSetMoney;
+        
+        public UnityEvent OnVisibleMoneyChanged;
+        public UnityEvent OnVisibleMoneyIncreased;
+        public UnityEvent OnVisibleMoneyDecreased;
+
         protected void Start()
         {
             if (type == Types.GameMoney)
             {
-                GlobalActions.OnGameMoneyChanged+=OnMoneyChanged;
+                GlobalActions.OnGameMoneyChanged += OnMoneyChanged;
             }
-            else if(type==Types.LevelMoney && LevelMoneyManager.Instance!=null)
-            {
-                GlobalActions.OnLevelMoneyChanged+=OnMoneyChanged;
-                
-            }else if(type==Types.TotalMoney && LevelMoneyManager.Instance!=null)
+            else if (type == Types.LevelMoney && LevelMoneyManager.Instance != null)
             {
                 GlobalActions.OnLevelMoneyChanged += OnMoneyChanged;
-                GlobalActions.OnGameMoneyChanged += OnMoneyChanged;                
+
+            }
+            else if (type == Types.TotalMoney && LevelMoneyManager.Instance != null)
+            {
+                GlobalActions.OnLevelMoneyChanged += OnMoneyChanged;
+                GlobalActions.OnGameMoneyChanged += OnMoneyChanged;
             }
 
             UpdateMoneyText();
         }
 
-        
-        public void SetOffset(float offset){
-            this.offset=offset;
+
+        public void SetOffset(float offset)
+        {
+            this.offset = offset;
             UpdateMoneyText();
         }
 
         public virtual void UpdateMoneyText()
         {
-             if (type == Types.GameMoney)
+            float moneyToSet = 0;
+            if (type == Types.GameMoney)
             {
-                textController.SetText(CoreManager.Instance.GameMoney+offset);
+                moneyToSet = CoreManager.Instance.GameMoney + offset;
             }
-            else if(type==Types.LevelMoney && LevelMoneyManager.Instance!=null)
+            else if (type == Types.LevelMoney && LevelMoneyManager.Instance != null)
             {
-                textController.SetText(LevelMoneyManager.Instance.LevelMoney+offset);
-                
-            }else if(type==Types.TotalMoney && LevelMoneyManager.Instance!=null)
+                moneyToSet = LevelMoneyManager.Instance.LevelMoney + offset;
+
+            }
+            else if (type == Types.TotalMoney && LevelMoneyManager.Instance != null)
             {
-                textController.SetText(LevelMoneyManager.Instance.LevelMoney+CoreManager.Instance.GameMoney+offset);
-            }            
+                moneyToSet = LevelMoneyManager.Instance.LevelMoney + CoreManager.Instance.GameMoney + offset;
+            }
+
+            textController.SetText(moneyToSet);
+            if (moneyToSet > _lastSetMoney)
+            {
+                OnVisibleMoneyIncreased?.Invoke();
+            }
+            else if (moneyToSet < _lastSetMoney)
+            {
+                OnVisibleMoneyDecreased?.Invoke();
+            }
+
+            if (moneyToSet != _lastSetMoney)
+            {
+                OnVisibleMoneyChanged?.Invoke();
+            }
+
+            _lastSetMoney = moneyToSet;
         }
 
         protected virtual void OnMoneyChanged(float amount)
         {
             UpdateMoneyText();
-        }  
+        }
 
         void OnDestroy()
         {
@@ -70,11 +98,11 @@ namespace CorePublic.UI
             {
                 GlobalActions.OnGameMoneyChanged -= OnMoneyChanged;
             }
-            else if (type == Types.LevelMoney && LevelMoneyManager.Instance!=null)
+            else if (type == Types.LevelMoney && LevelMoneyManager.Instance != null)
             {
                 GlobalActions.OnLevelMoneyChanged -= OnMoneyChanged;
             }
-            else if (type == Types.TotalMoney && LevelMoneyManager.Instance!=null)
+            else if (type == Types.TotalMoney && LevelMoneyManager.Instance != null)
             {
                 GlobalActions.OnLevelMoneyChanged -= OnMoneyChanged;
                 GlobalActions.OnGameMoneyChanged -= OnMoneyChanged;
